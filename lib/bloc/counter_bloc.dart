dc5a9d5e23c46019f6b132c19b113c58586052d7
Sabiq Mohammed <sabiq.mohammed4@gmail.com>
2024-09-12 14:48:22 +0530

    counter increment

---
+++ lib/bloc/counter_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc_cubit/bloc/counter_event.dart';
+import 'package:bloc_cubit/bloc/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterIncrementEvent, CounterState> {
+  CounterBloc() : super(CounterState(count: 0)) {
+    on<CounterIncrementEvent>(
+      (event, emit) {
+        emit(CounterState(count: state.count + 1));
+      },
+    );
+  }
+}
